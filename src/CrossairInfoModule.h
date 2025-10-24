02dd9948a804d1d4b491d821534ec0a65d08a03d
Han Programer <30620423+Hanprogramer@users.noreply.github.com>
2025-10-24 13:16:18 +0700

    Refactor: Separate crossair info to its own namespace

---
+++ src/CrossairInfoModule.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <Windows.h>
+#include <amethyst/runtime/mod/Mod.hpp>
+#include <amethyst/runtime/ModContext.hpp>
+#include <amethyst/runtime/events/GameEvents.hpp> // OnStartJoinGameEvent
+#include <amethyst/runtime/events/RenderingEvents.hpp>
+#include <amethyst/ui/NinesliceHelper.hpp>
+#include <mc/src-client/common/client/gui/ScreenView.hpp>
+#include <mc/src-client/common/client/gui/gui/VisualTree.hpp>
+#include <mc/src-client/common/client/gui/gui/UIControl.hpp>
+#include <mc/src-client/common/client/gui/gui/GuiData.hpp>
+#include <mc/src-client/common/client/game/ClientInstance.hpp>
+#include <mc/src-client/common/client/gui/gui/FontHandle.hpp>
+#include <mc/src-deps/core/utility/NonOwnerPointer.hpp>
+#include <mc/src-client/common/client/player/LocalPlayer.hpp>
+#include <mc/src/common/world/level/BlockSource.hpp>
+#include <mc/src/common/CommonTypes.hpp>
+#include <mc/src/common/world/level/block/registry/BlockTypeRegistry.hpp>
+#include <mc/src/common/locale/I18n.hpp>
+#include <mc/src-deps/core/string/StringHash.hpp>
+
+#include <algorithm>
+
+namespace CrossairInfoModule
+{
+	void Init();
+	void OnAfterRenderUi(AfterRenderUIEvent event);
+	std::string getModNameFromNamespace(std::string mNamespace);
+};
+
--- src/dllmain.hpp
+++ src/dllmain.hpp
@@ -3,28 +3,12 @@
 #include <amethyst/runtime/mod/Mod.hpp>
 #include <amethyst/runtime/ModContext.hpp>
 #include <amethyst/runtime/events/GameEvents.hpp> // OnStartJoinGameEvent
-#include <string>
-#include <amethyst/runtime/events/RenderingEvents.hpp>
-#include <amethyst/ui/NinesliceHelper.hpp>
-#include <mc/src-client/common/client/gui/ScreenView.hpp>
-#include <mc/src-client/common/client/gui/gui/VisualTree.hpp>
-#include <mc/src-client/common/client/gui/gui/UIControl.hpp>
-#include <mc/src-client/common/client/gui/gui/GuiData.hpp>
-#include <mc/src-client/common/client/game/ClientInstance.hpp>
-#include <mc/src-client/common/client/gui/gui/FontHandle.hpp>
-#include <mc/src-deps/core/utility/NonOwnerPointer.hpp>
-#include <mc/src-client/common/client/player/LocalPlayer.hpp>
-#include <mc/src/common/world/level/BlockSource.hpp>
-#include <mc/src/common/CommonTypes.hpp>
-#include <mc/src/common/world/level/block/registry/BlockTypeRegistry.hpp>
-#include <mc/src/common/locale/I18n.hpp>
-#include <mc/src-deps/core/string/StringHash.hpp>
-#define ModFunction extern "C" __declspec(dllexport)
+#include "dllmain.hpp"
+#include "CrossairInfoModule.h"
 
+#define ModFunction extern "C" __declspec(dllexport)
 
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     return TRUE;
 }
-
-void OnAfterRenderUI(AfterRenderUIEvent& event);
